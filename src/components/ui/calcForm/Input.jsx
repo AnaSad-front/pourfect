872bd34f23782a55b97723a466c1698feb72f64d
AnaSad-front <anastasiasadyreva1@gmail.com>
2026-01-28 16:36:18 +0300

    fix: input text size

--- src/components/ui/calcForm/Input.jsx
+++ src/components/ui/calcForm/Input.jsx
@@ -14,7 +14,7 @@ function Input({
       <input
         type={type}
         pattern="[0-9]*"
-        className="rounded-lg cursor-pointer outline outline-accent p-xxs w-full text-primary text-xs placeholder:text-secondary hover:bg-bg-secondary hover:outline-accent-secondary focus:outline-accent-secondary active:bg-bg-secondary"
+        className="rounded-lg cursor-pointer outline outline-accent p-xxs w-full text-primary text-sm placeholder:text-secondary hover:bg-bg-secondary hover:outline-accent-secondary focus:outline-accent-secondary active:bg-bg-secondary"
         placeholder={placeholder}
         value={value}
         onChange={onChange}
